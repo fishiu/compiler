a3d271860b1e2757af1f41237ca0df160c7e4301
Xiaoyuan Jin <1800016611@pku.edu.cn>
2022-06-06 05:03:27 +0800

    lv3.1 riscv

--- src/ir.cpp
+++ src/ir.cpp
@@ -1,21 +1,24 @@
 #include <ir.hpp>
+#include <map>
 
+int reg_cnt = 0;
+std::map<const koopa_raw_value_t, std::string> vmap;
 
-void gen_riscv (std::string koopa_str) {
-    koopa_program_t program;
-    koopa_error_code_t ret = koopa_parse_from_string(koopa_str.c_str(), &program);
-    assert(ret == KOOPA_EC_SUCCESS);
-    
-    koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
-    koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
-    koopa_delete_program(program);
+void gen_riscv(std::string koopa_str) {
+  printf("%s\n", koopa_str.c_str());
+  koopa_program_t program;
+  koopa_error_code_t ret = koopa_parse_from_string(koopa_str.c_str(), &program);
+  assert(ret == KOOPA_EC_SUCCESS);
 
-    // handle raw before delete
-    Visit(raw);
+  koopa_raw_program_builder_t builder = koopa_new_raw_program_builder();
+  koopa_raw_program_t raw = koopa_build_raw_program(builder, program);
+  koopa_delete_program(program);
 
-    koopa_delete_raw_program_builder(builder);
+  // handle raw before delete
+  Visit(raw);
+
+  koopa_delete_raw_program_builder(builder);
 }
-
 
 // visit raw program
 void Visit(const koopa_raw_program_t &program) {
@@ -30,6 +33,7 @@ void Visit(const koopa_raw_program_t &program) {
 
 // visit raw slice
 void Visit(const koopa_raw_slice_t &slice) {
+  printf("visit slice\n");
   for (size_t i = 0; i < slice.len; ++i) {
     auto ptr = slice.buffer[i];
     switch (slice.kind) {
@@ -50,23 +54,27 @@ void Visit(const koopa_raw_slice_t &slice) {
 
 // visit func
 void Visit(const koopa_raw_function_t &func) {
-  // remove @
-  std::string func_name = std::string(func->name);
-  func_name = func_name.substr(1);
-  
-  std::cout << func_name << ":" << std::endl;
+  printf("visit func\n");
+  std::cout << func->name + 1 << ":" << std::endl;
   // visit all basic blocks
   Visit(func->bbs);
 }
 
 // visit basic block
 void Visit(const koopa_raw_basic_block_t &bb) {
+  printf("visit bb\n");
   Visit(bb->insts);
 }
 
 // visit value
-void Visit(const koopa_raw_value_t &value) {
+std::string Visit(const koopa_raw_value_t &value) {
+  if (vmap[value] != "") {
+    return vmap[value];
+  }
+
+  printf("visit value\n");
   const auto &kind = value->kind;
+  std::string reg;
   switch (kind.tag) {
     case KOOPA_RVT_RETURN:
       // 访问 return 指令
@@ -74,29 +82,61 @@ void Visit(const koopa_raw_value_t &value) {
       break;
     case KOOPA_RVT_INTEGER:
       // 访问 integer 指令
-      Visit(kind.data.integer);
+      reg = Visit(kind.data.integer);
+      break;
+    case KOOPA_RVT_BINARY:
+      reg = Visit(kind.data.binary);
       break;
     default:
       // 其他类型暂时遇不到
       assert(false);
   }
+  
+  vmap[value] = reg;
+  return reg;
 }
-
 
 // visit return
 void Visit(const koopa_raw_return_t &ret) {
-  std::cout << "  li a0, ";
-  Visit(ret.value);
-
+  printf("visit return\n");
+  std::string reg = Visit(ret.value);
+  std::cout << "  # ret" << std::endl;
+  std::cout << "  mv a0, " << reg << std::endl;
   std::cout << "  ret" << std::endl;
 }
 
-
-void Visit(const koopa_raw_integer_t &integer) {
-  std::cout << integer.value << std::endl;
+// visit integer
+std::string Visit(const koopa_raw_integer_t &integer) {
+  printf("visit integer\n");
+  if (integer.value == 0)
+    return "x0";
+  std::string reg = "t" + std::to_string(reg_cnt++);
+  std::cout << "  # li integer" << std::endl;
+  std::cout << "  " << "li " << reg << ", " << integer.value << std::endl;
+  return reg;
 }
 
+// visit binary expression
+std::string Visit(const koopa_raw_binary_t &binary) {
+  printf("visit binary\n");
+  koopa_raw_binary_op_t op = binary.op;
+  std::string left = Visit(binary.lhs);
+  std::string right = Visit(binary.rhs);
+  std::string reg = "t" + std::to_string(reg_cnt++);
 
-void Visit(const koopa_raw_aggregate_t &agg) {
-  assert(false);
+  switch (op) {
+    case KOOPA_RBO_EQ:  // eq
+      std::cout << "  # eq" << std::endl;
+      std::cout << "  xor " << reg << ", " << left << ", " << right << std::endl;
+      std::cout << "  seqz " << reg << ", " << left << std::endl;
+      break;
+    case KOOPA_RBO_SUB:  // sub
+      std::cout << "  # sub" << std::endl;
+      std::cout << "  sub " << reg << ", " << left << ", " << right << std::endl;
+      break;
+    default:
+      assert(false);
+  }
+
+  return reg;
 }
--- src/ir.hpp
+++ src/ir.hpp
@@ -11,10 +11,10 @@ void Visit(const koopa_raw_program_t &program);
 void Visit(const koopa_raw_slice_t &slice);
 void Visit(const koopa_raw_function_t &func);
 void Visit(const koopa_raw_basic_block_t &bb);
-void Visit(const koopa_raw_value_t &value);
-
 void Visit(const koopa_raw_return_t &ret);
-void Visit(const koopa_raw_integer_t &integer);
-void Visit(const koopa_raw_aggregate_t &agg);
+// return reg name
+std::string Visit(const koopa_raw_value_t &value);
+std::string Visit(const koopa_raw_integer_t &integer);
+std::string Visit(const koopa_raw_binary_t &binary);
 
 #endif
