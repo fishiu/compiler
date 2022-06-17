335b6989d5f1023577c13f30e0c31a9d2e68e05a
Xiaoyuan Jin <1800016611@pku.edu.cn>
2022-06-17 14:25:37 +0800

    lv4 riscv: with bug (11/14 on test)

--- src/ir.cpp
+++ src/ir.cpp
@@ -1,20 +1,73 @@
 #include <ir.hpp>
 #include <map>
+#include <vector>
 #include <cmath>
 
 using namespace std;
 
 // some global variables
-int stack_size = 0;  // stack length
-int reg_cnt = 0;
-int int_reg_cnt = 0;  // int_reg will be cleaned after visiting a value
+class Stack {
+ private:
+  int stack_top = 0;   // stack top position
+  int stack_size = 0;  // stack length
+ public:
+  int inc_top(int size) {
+    printf("[debug stack] inc_stack: %d\n", size);
+    return stack_top += size;
+  }
+  int get_top() {
+    return stack_top;
+  }
+
+  int get_size() {
+    return stack_size;
+  }
+  void set_size(int size) {
+    printf("[debug stack] set_size: %d\n", size);
+    stack_size = size;
+  }
+} stack;
+
+class RegAllocator {
+ private:
+  // todo consider changing the regs into queue or other data structure to avoid loops
+  vector<reg_t> regs;
+ 
+ public:
+  RegAllocator() {
+    for (int i = 0; i < REGNUM; ++i) {
+      reg_t reg = {i, false};
+      regs.push_back(reg);
+    }
+  }
+
+  const reg_t& alloc() {
+    for (auto &reg : regs) {
+      if (!reg.occupied) {
+        // found an available registor
+        reg.occupied = true;
+        return reg;
+      }
+    }
+    // no available register
+    assert(false);
+  }
+
+  void free() {
+    for (auto &reg : regs) {
+      reg.occupied = false;
+    }
+  }
+} reg_allocator;
 
 string format_reg(int reg_num) {
   string reg_str;
   if (reg_num < 7) {  // t0 ~ t6
     reg_str = "t" + to_string(reg_num);
-  } else if (reg_num < 15) {  // a0 ~ a7
+  } else if (reg_num < REGNUM) {  // a0 ~ a7
     reg_str = "a" + to_string(reg_num - 7);
+  } else if (reg_num == REGNUM) {  // x0
+    reg_str = "x0";
   } else {
     assert(false);
   }
@@ -23,7 +76,7 @@ string format_reg(int reg_num) {
 
 // must use a value map, so when referred to a value pointer
 // it won't be dump twice
-map<const koopa_raw_value_t, string> vmap;
+map<const koopa_raw_value_t, repr_t> vmap;
 
 string get_op_str(koopa_raw_binary_op_t op) {
   switch (op) {
@@ -103,6 +156,7 @@ void Visit(const koopa_raw_function_t &func) {
   printf("visit func\n");
   cout << func->name + 1 << ":" << endl;
   // get stack size
+  int stack_size = 0;
   for (size_t i = 0; i < func->bbs.len; ++i) {
     koopa_raw_basic_block_t bb_ptr = reinterpret_cast<koopa_raw_basic_block_t>(func->bbs.buffer[i]);
     for (size_t j = 0; j < bb_ptr->insts.len; ++j) {
@@ -113,10 +167,11 @@ void Visit(const koopa_raw_function_t &func) {
     }
   }
   stack_size = ceil(stack_size / 16.0) * 16;
+  stack.set_size(stack_size);
   if (stack_size <= 2048)
-    cout << "  addi sp, sp, " << to_string(-stack_size) << endl;
+    cout << "  addi sp, sp, " << -stack_size << endl;
   else {
-    cout << "  li t0, " << to_string(-stack_size) << endl;
+    cout << "  li t0, " << -stack_size << endl;
     cout << "  addi sp, sp, t0" << endl;
   }
   // visit all basic blocks
@@ -130,66 +185,133 @@ void Visit(const koopa_raw_basic_block_t &bb) {
 }
 
 // visit value
-string Visit(const koopa_raw_value_t &value) {
-  if (vmap[value] != "") {
-    return vmap[value];
+repr_t Visit(const koopa_raw_value_t &value) {
+  if (vmap.count(value)) {
+    // do not use reference here, no need to change stored value
+    repr_t repr = vmap[value];
+    if (repr.is_reg) {
+      return repr;
+    } else {
+      // no reg binded but has addr in stack
+      assert(repr.addr != -1);
+      reg_t reg = reg_allocator.alloc();
+      cout << "  lw " << format_reg(reg.regid) << ", " << repr.addr << "(sp)" << endl;
+      repr.is_reg = true;
+      repr.addr = reg.regid;
+    }  // already has reg bound to value
+    return repr;
   }
 
-  printf("visit value\n");
+  printf("visit new value\n");
   const auto &kind = value->kind;
-  string reg;
+  repr_t repr = {false, -1};
   switch (kind.tag) {
     case KOOPA_RVT_RETURN:
-      // 访问 return 指令
+      cout << "\n  # ret" << endl;
       Visit(kind.data.ret);
+      reg_allocator.free();
       break;
     case KOOPA_RVT_INTEGER:
-      // 访问 integer 指令
-      reg = Visit(kind.data.integer);
+      repr = Visit(kind.data.integer);
+      // do not save
       break;
     case KOOPA_RVT_BINARY:
-      reg = Visit(kind.data.binary);
+      cout << "\n  # binary" << endl;
+      repr = Visit(kind.data.binary);
+      assert(!repr.is_reg);
+      vmap[value] = repr;
+      reg_allocator.free();
+      break;
+    case KOOPA_RVT_ALLOC:
+      cout << "\n  # alloc" << endl;
+      repr.addr = stack.get_top();
+      stack.inc_top(4);
+      vmap[value] = repr;
+      reg_allocator.free();
+      break;
+    case KOOPA_RVT_LOAD:
+      cout << "\n  # load" << endl;
+      repr = Visit(kind.data.load);
+      assert(!repr.is_reg);
+      vmap[value] = repr;
+      reg_allocator.free();
+      break;
+    case KOOPA_RVT_STORE:
+      cout << "\n  # store" << endl;
+      Visit(kind.data.store);
+      reg_allocator.free();
       break;
     default:
       // 其他类型暂时遇不到
+      printf("unhandled value kind %d\n", kind.tag);
       assert(false);
   }
 
-  vmap[value] = reg;
-  return reg;
+  return repr;
 }
 
 // visit return
 void Visit(const koopa_raw_return_t &ret) {
   printf("visit return\n");
-  string reg = Visit(ret.value);
-  cout << "  # ret" << endl;
-  cout << "  mv a0, " << reg << endl;
+  repr_t repr = Visit(ret.value);
+
+  // cout << "  # ret" << endl;
+  assert(repr.is_reg);
+  cout << "  mv a0, " << format_reg(repr.addr) << endl;
+  int stack_size = stack.get_size();
+  if (stack_size <= 2048)
+    cout << "  addi sp, sp, " << stack_size << endl;
+  else {
+    // too big stack size
+    cout << "  li t0, " << stack_size << endl;
+    cout << "  addi sp, sp, t0" << endl;
+  }
   cout << "  ret" << endl;
 }
 
 // visit integer
-string Visit(const koopa_raw_integer_t &integer) {
+repr_t Visit(const koopa_raw_integer_t &integer) {
   printf("visit integer\n");
-  if (integer.value == 0)
-    return "x0";
-  string reg = format_reg(int_reg_cnt++);
-  cout << "  # li integer" << endl;
-  cout << "  " << "li " << reg << ", " << integer.value << endl;
-  return reg;
+  repr_t repr = {true, -1};
+
+  if (integer.value == 0) {
+    repr.addr = REGNUM;  // x0
+    return repr;
+  }
+  reg_t reg = reg_allocator.alloc();  // only interger won't occupy
+  repr.addr = reg.regid;
+  cout << "  li " << format_reg(repr.addr) << ", " << integer.value << endl;
+  
+  return repr;
 }
 
 // visit binary expression
-string Visit(const koopa_raw_binary_t &binary) {
+repr_t Visit(const koopa_raw_binary_t &binary) {
   printf("visit binary\n");
   koopa_raw_binary_op_t op = binary.op;
   // assume: only int will assign new reg when Visit(koopa_value)
-  int_reg_cnt = reg_cnt;
-  string left = Visit(binary.lhs);
-  string right = Visit(binary.rhs);
-  int_reg_cnt = reg_cnt;  // restore int_reg_cnt
-  string reg = format_reg(reg_cnt++);
+  repr_t left = Visit(binary.lhs);
+  repr_t right = Visit(binary.rhs);
+  repr_t result = {true, reg_allocator.alloc().regid};
+
   string op_str;
+  // load left
+  if (!left.is_reg) {
+    reg_t reg = reg_allocator.alloc();
+    cout << "  lw " << format_reg(reg.regid) << ", " << left.addr << "(sp)" << endl;
+    left.is_reg = true;
+    left.addr = reg.regid;
+  }
+  // load right
+  if (!right.is_reg) {
+    reg_t reg = reg_allocator.alloc();
+    cout << "  lw " << format_reg(reg.regid) << ", " << right.addr << "(sp)" << endl;
+    right.is_reg = true;
+    right.addr = reg.regid;
+  }
+  string left_reg = format_reg(left.addr);
+  string right_reg = format_reg(right.addr);
+  string result_reg = format_reg(result.addr);
   
   switch (op) {
     case KOOPA_RBO_ADD:  // add
@@ -203,28 +325,65 @@ string Visit(const koopa_raw_binary_t &binary) {
     case KOOPA_RBO_GT:   // sgt
       op_str = get_op_str(op);
       cout << "  # " << op_str << endl;
-      cout << "  " << op_str << " " << reg << ", " << left << ", " << right << endl;
+      cout << "  " << op_str << " " << result_reg << ", " << left_reg << ", " << right_reg << endl;
       break;
     case KOOPA_RBO_EQ:  // eq
       cout << "  # eq" << endl;
-      cout << "  xor " << reg << ", " << left << ", " << right << endl;
-      cout << "  seqz " << reg << ", " << reg << endl;
+      cout << "  xor " << result_reg << ", " << left_reg << ", " << right_reg << endl;
+      cout << "  seqz " << result_reg << ", " << result_reg << endl;
       break;
     case KOOPA_RBO_NOT_EQ:  // neq
-      cout << "  xor " << reg << ", " << left << ", " << right << endl;
-      cout << "  snez " << reg << ", " << reg << endl;
+      cout << "  xor " << result_reg << ", " << left_reg << ", " << right_reg << endl;
+      cout << "  snez " << result_reg << ", " << result_reg << endl;
       break;
     case KOOPA_RBO_LE:  // le
-      cout << "  sgt " << reg << ", " << left << ", " << right << endl;
-      cout << "  xori " << reg << ", " << reg << ", 1" << endl; 
+      cout << "  sgt " << result_reg << ", " << left_reg << ", " << right_reg << endl;
+      cout << "  xori " << result_reg << ", " << result_reg << ", 1" << endl; 
       break;
     case KOOPA_RBO_GE:  // ge
-      cout << "  slt " << reg << ", " << left << ", " << right << endl;
-      cout << "  xori " << reg << ", " << reg << ", 1" << endl; 
+      cout << "  slt " << result_reg << ", " << left_reg << ", " << right_reg << endl;
+      cout << "  xori " << result_reg << ", " << result_reg << ", 1" << endl; 
       break;
     default:
       assert(false);
   }
 
-  return reg;
+  result.is_reg = false;
+  result.addr = stack.get_top();
+  stack.inc_top(4);
+  cout << "  sw " << result_reg << ", " << result.addr << "(sp)" << endl;
+
+  return result;
+}
+
+// load value from src
+repr_t Visit(const koopa_raw_load_t &load) {
+  printf("visit load\n");
+  koopa_raw_value_t src = load.src;
+  repr_t dest = {true, reg_allocator.alloc().regid};
+  // save the load value in reg, ready to return
+  repr_t src_repr = Visit(src);
+  
+  assert(src_repr.is_reg);
+  string reg_name = format_reg(dest.addr);
+  string src_reg_name = format_reg(src_repr.addr);
+
+  // store the reg content into stack
+  dest = {false, stack.get_top()};
+  stack.inc_top(4);
+  cout << "  sw " << reg_name << ", " << dest.addr << "(sp)" << endl;
+
+  return dest;
+}
+
+void Visit(const koopa_raw_store_t &store) {
+  repr_t repr = Visit(store.value);
+  koopa_raw_value_t dest = store.dest;
+  assert(vmap.count(dest));
+  repr_t dest_repr = vmap[dest];
+  assert(!dest_repr.is_reg);
+
+  assert(repr.is_reg);
+  string reg_name = format_reg(repr.addr);
+  std::cout << "  sw " << reg_name << ", " << dest_repr.addr << "(sp)" << std::endl;
 }
--- src/ir.hpp
+++ src/ir.hpp
@@ -5,6 +5,20 @@
 #include <cassert>
 #include <iostream>
 #include <koopa.h>
+#define REGNUM 15
+
+// a struct to save the return of a koopa value
+typedef struct {
+  bool is_reg;
+  int addr;
+} repr_t;
+
+// register struct
+typedef struct {
+  int regid;
+  bool occupied;
+  // koopa_raw_value_t target;
+} reg_t;
 
 void gen_riscv(std::string koopa_str);
 void Visit(const koopa_raw_program_t &program);
@@ -12,9 +26,11 @@ void Visit(const koopa_raw_slice_t &slice);
 void Visit(const koopa_raw_function_t &func);
 void Visit(const koopa_raw_basic_block_t &bb);
 void Visit(const koopa_raw_return_t &ret);
+void Visit(const koopa_raw_store_t &store);
 // return reg name
-std::string Visit(const koopa_raw_value_t &value);
-std::string Visit(const koopa_raw_integer_t &integer);
-std::string Visit(const koopa_raw_binary_t &binary);
+repr_t Visit(const koopa_raw_value_t &value);
+repr_t Visit(const koopa_raw_integer_t &integer);
+repr_t Visit(const koopa_raw_binary_t &binary);
+repr_t Visit(const koopa_raw_load_t &load);
 
 #endif
